e05cdb43e90d5a0bf4a9b0c0ddc565fad535614f
MINJUNG KIM <85140461+santos019@users.noreply.github.com>
2024-06-20 22:23:58 +0900

    Time: 560 ms (23.01%), Space: 54.4 MB (5.36%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set <String> noDuplicate = new HashSet();
+
+        Arrays.sort(nums);
+        int right = 0;
+        int left = 0;
+        int fix = 0;
+
+        for(int i=0; i<nums.length - 2; i++) {
+            fix = i;
+            left  = i + 1;
+            right = nums.length - 1;
+            while(right > left && fix < left) {
+                if(nums[fix] + nums[right] + nums[left] > 0) {
+                    right --;
+                } else if (nums[fix] + nums[right] + nums[left] < 0 ) {
+                    left ++;
+                } else {
+                    if(!noDuplicate.contains(nums[fix] + "/" + nums[left] + "/" + nums[right]))
+                    {
+                        res.add(List.of(nums[fix], nums[left], nums[right]));
+                        noDuplicate.add(nums[fix] + "/" + nums[left] + "/" + nums[right]);
+                    }
+                    
+                    right --;
+                    left ++;
+                }
+            }
+
+        } 
+        return res;
+    }
+}

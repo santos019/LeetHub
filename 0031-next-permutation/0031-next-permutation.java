eea6771582978e34a93d9ff0d33883960da3599d
MINJUNG KIM <85140461+santos019@users.noreply.github.com>
2024-10-27 22:59:52 +0900

    Time: 1 ms (14.10%), Space: 43.4 MB (7.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,46 +1,71 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int len = nums.length;
-        int spot = -1;
-        
-        if(len == 1) return;
-        
-        int current = nums[len - 1];
-        int min = current;
-        for(int i = len-2; i > -1; i--) {
-            int before = nums[i];
-            if(current > before) {
-                spot = i;
-                break;
-            }
-            current = before;
-        }
-        
-        if(spot == -1) {
-            reverse(nums, 0 , len -1);
-            return;
-        }
-        
-        for(int j = len -1; j > -1; j--) {
-            if(nums[j] > nums[spot]) {
-                int changedNum = nums[spot];
-                nums[spot] = nums[j];
-                nums[j] = changedNum;
-                break;
-            }
-        }
-        Arrays.sort(nums, spot + 1, len);
 
-        return;
+        int targetIndex = getTargetIndex(nums, len);
+        tryGetNextPermutation(targetIndex, nums, len);
+
     }
 
-    public void reverse(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    private int getTargetIndex(int[] nums, final int len) {
+        int beforeValue = nums[len-1];
+
+        for(int i = len - 2; i >= 0; i--) {
+            if(nums[i] >= beforeValue) {
+                beforeValue = nums[i];
+                continue;
+            }
+
+            if(nums[i] < beforeValue) {
+                return i;
+            }
         }
+
+        return -1;
     }
+
+    private void tryGetNextPermutation (final int targetIndex, int[] nums, final int len) {
+        if(targetIndex == -1) {
+            canNotGetNextPermutation(nums);
+        }
+
+        if(targetIndex != -1) {
+            canGetNextPermutation(targetIndex, nums, len);
+        }
+
+    }
+
+    private void canNotGetNextPermutation(int[] nums) {
+        Arrays.sort(nums);
+
+    }
+
+    private void canGetNextPermutation(final int targetIndex, int[] nums, final int len) {
+        int min = nums[targetIndex + 1];
+        int minIndex = getMinIndex(nums, targetIndex + 2, len, targetIndex, min);
+
+        swap(nums, minIndex, targetIndex);
+        Arrays.sort(nums, targetIndex + 1, len);
+
+    }
+
+    private int getMinIndex(int[] nums, final int startIndex, final int endIndex, final int targetIndex, int min) {
+        int minIndex = targetIndex + 1;
+        for(int i = startIndex; i < endIndex; i++) {
+            if(min > nums[i] && nums[i] > nums[targetIndex]) {
+                min = nums[i];
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
+    private void swap(int[] nums, final int minIndex, final int targetIndex) {
+        int tmp = nums[minIndex];
+        nums[minIndex] = nums[targetIndex];
+        nums[targetIndex] = tmp;
+
+    }
+
 }

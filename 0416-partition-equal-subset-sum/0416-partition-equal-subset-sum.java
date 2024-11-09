c6bbab1e3196f56485749273c044dc90a9c2c8e4
MINJUNG KIM <85140461+santos019@users.noreply.github.com>
2024-11-09 17:02:10 +0900

    Time: 26 ms (82.39%), Space: 42.1 MB (87.69%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,27 +1,28 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int cost = 0;
         int len = nums.length;
-
-        for(int i = 0; i <len; i ++) {
-            cost = cost + nums[i];        
-        }
-
-        if(cost%2 != 0) return false;
+        int totalSum = 0;
+        int target = 0;
         
-        return canMakeTarget(nums, cost/2);
-    }
-
-    public boolean canMakeTarget(int[] nums, int target) {
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;  
-
-         for (int num : nums) {
-            for (int i = target; i >= num; i--) {
-                dp[i] = dp[i] || dp[i - num];
+        for(int num : nums) {
+            totalSum = totalSum + num;
+        }
+        
+        if (totalSum%2 != 0) {
+            return false;
+        }
+        
+        target = totalSum/2;
+        
+        boolean[] partitionBook = new boolean[target + 1];
+        partitionBook[0] = true;
+        
+        for(int num : nums) {
+            for(int i = target; i >= num; i--) {
+                partitionBook[i] = partitionBook[i] || partitionBook[i - num];
             }
         }
-        
-        return dp[target];
+    
+        return partitionBook[target];
     }
 }
